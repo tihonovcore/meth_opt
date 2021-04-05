eb2288e0090c654db220fb94a096e95f17ec63e7
tihonovcore <tihonovcore@gmail.com>
2021-04-05 20:02:16 +0300

    [lab 3] add naive `f(x0)` evaluation

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
-import numpy as np
-
 import simplex
 
 from tables.tables import all_basics, all_tables
 
-# todo: get from `create_table` or by algorithm
-xs = [np.array([0, 0, 40, 30, 60, 80]), np.array([0, 0, 4, 2, 5]), np.array([0, 0, 0, 0, 4, 4])]
-
-for table, basic, x in zip(all_tables, all_basics, xs):
-    table[-1][0] = x @ table[-1][1:]
+for table, basic in zip(all_tables, all_basics):
     print(simplex.simplex(table, basic))
--- src/tables/create_table.py
+++ src/tables/create_table.py
@@ -44,21 +44,34 @@ def to_table_format(constraint):
     return [constraint[-1], *constraint[:-2]]
 
 
+def evaluate_function(old_vars_count, new_vars_count, constraints, function):
+    x = [0 for _ in range(old_vars_count + new_vars_count)]
+
+    for constraint in constraints:
+        unit_vector = constraint[old_vars_count:-2]
+        index_at_eye = np.argmax(unit_vector)
+
+        if unit_vector[index_at_eye] == 1:
+            x[old_vars_count + index_at_eye] = constraint[-1]
+
+    return np.array(function) @ np.array(x)
+
+
 # constrains is list like: [a_i_1, .., a_i_m, ###, b_i], where ### is "==" or "<=" or ">="
 # function   is list like: [c_1, .., c_m]
 def create_table(constraints, function):
     constraints = list(map(change_ge_to_le, constraints))
 
-    old_vars_count = len(function) + 1
+    old_vars_count = len(function)
     new_vars_count = less_or_equals_count(constraints)
     add_new_variables(constraints, new_vars_count)
     function.extend([0 for _ in range(new_vars_count)])
 
-    # todo: find x_0 and evaluate f(x_0)
-    function.insert(0, 0)
+    f_at_x0 = evaluate_function(old_vars_count, new_vars_count, constraints, function)
+    function.insert(0, f_at_x0)
 
     constraints = list(map(to_table_format, constraints))
     table = np.array([*constraints, function], dtype='float32')
-    basic = np.array([i for i in range(old_vars_count, old_vars_count + new_vars_count)])
+    basic = np.array([i for i in range(old_vars_count + 1, old_vars_count + 1 + new_vars_count)])
 
     return table, basic
