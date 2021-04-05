399c3c7ec526905e557dafbef02cc06e3706fb57
tihonovcore <tihonovcore@gmail.com>
2021-04-05 22:43:50 +0300

    [lab 3] add simple tests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import simplex
-from tables.create_table import create_simplex_table
 
+from tables.create_table import create_simplex_table
 from tables.tables import all_tables_descriptions
 
 for table_description in all_tables_descriptions:
---
+++ src/simplex_test.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+import simplex
+
+from tables.create_table import create_simplex_table
+from tables.tables import all_tables_descriptions
+
+
+def find_table_by_description(description):
+    for d in all_tables_descriptions:
+        if d.description == description:
+            return d
+
+    return None
+
+
+def test_from_book():
+    table_description = find_table_by_description("from book")
+    create_simplex_table(table_description)
+    f_opt, x_opt = simplex.simplex(table_description)
+
+    assert f_opt == 140.0
+    assert x_opt == [40.0, 20.0]
+
+
+def test_from_lecture():
+    table_description = find_table_by_description("from lecture")
+    create_simplex_table(table_description)
+    f_opt, x_opt = simplex.simplex(table_description)
+
+    assert f_opt == 7.0
+    assert x_opt == [3.0, 2.0]
+
+
+def test_from_lab_46():
+    table_description = find_table_by_description("from lab 4.6")
+    create_simplex_table(table_description)
+    f_opt, x_opt = simplex.simplex(table_description)
+
+    assert f_opt == -3.0
+    assert x_opt == [np.float32(i) for i in [8.0 / 3, 0, 0, 1.0 / 3]]
+
+
+if __name__ == '__main__':
+    test_from_book()
+    test_from_lecture()
+    test_from_lab_46()
